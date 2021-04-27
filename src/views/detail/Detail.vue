ffb570273e807e4929a9e70f62397fa6b3c5da19
LiuLei <Lei.Liu@imotion.ai>
2021-04-27 13:43:10 +0800

    step19: display of basic information of goods

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,14 +2,16 @@
   <div id="detail">
     <detail-nav-bar></detail-nav-bar>
     <detail-swiper :top-images="topImages"></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComponents/DetailNavBar";
 import DetailSwiper from "./childComponents/DetailSwiper";
+import DetailBaseInfo from "./childComponents/DetailBaseInfo";
 
-import { getDetail } from "network/detail";
+import { getDetail, Goods } from "network/detail";
 
 export default {
   name: "Detail",
@@ -17,11 +19,13 @@ export default {
     return {
       iid: null,
       topImages: [],
+      goods: {},
     };
   },
   components: {
     DetailNavBar,
     DetailSwiper,
+    DetailBaseInfo,
   },
   created() {
     // 1.保存传入的商品iid
@@ -29,9 +33,13 @@ export default {
 
     // 2.根据对应的iid请求对应的详情数据
     getDetail(this.iid).then((res) => {
-      // console.log(res);
+      console.log(res);
+      const data = res.result;
       // 2.1 先获取顶部轮播图数据
-      this.topImages = res.result.itemInfo.topImages;
+      this.topImages = data.itemInfo.topImages;
+
+      // 2.2 获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
     });
   },
 };
