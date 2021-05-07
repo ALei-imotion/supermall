a24e5afff496c28f58cc4f9b4d765725780844cb
LiuLei <Lei.Liu@imotion.ai>
2021-05-07 17:24:20 +0800

    step33: display of shopping cart product list

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="cart">
+    <!-- 导航栏 -->
     <nav-bar class="cart-nav-bar">
-      <div slot="middle">购物车({{length}})</div>
+      <div slot="middle">购物车({{ length }})</div>
     </nav-bar>
+
+    <!-- 商品列表 -->
+    <cart-list></cart-list>
+
+    <!-- 底部汇总 -->
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
-import { mapGetters } from "vuex";  // mapGetters 辅助函数仅仅是将store中的getter映射到局部计算属性
+import CartList from "./childComponents/CartList";
+
+import { mapGetters } from "vuex"; // mapGetters 辅助函数仅仅是将store中的getter映射到局部计算属性
 
 export default {
   name: "Cart",
   components: {
     NavBar,
+    CartList,
   },
   computed: {
     // 第一种语法，指定需要将哪些getters转换成计算属性（数组方式，表示是一一对应关系，名字是不变的）
@@ -25,6 +34,10 @@ export default {
 </script>
 
 <style scoped>
+.cart {
+  height: 100vh;
+}
+
 .cart-nav-bar {
   background-color: var(--color-tint);
   color: #fff;
